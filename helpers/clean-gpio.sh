00e63a501d63c19cf70348513c98f215fb0a9762
michalina kaszuba <kaszuba.mika@gmail.com>
2025-08-10 21:28:49 +0200

    clean-up

--- helpers/clean-gpio.sh
+++
@@ -1,5 +0,0 @@
-python3 -c "import RPi.GPIO as GPIO; GPIO.cleanup()"
-#Test basic GPIO:
-python3 -c "import RPi.GPIO as GPIO; print('GPIO import successful')"
-#Check if pin is stuck:
-sudo python3 -c "import RPi.GPIO as GPIO; GPIO.setmode(GPIO.BCM); GPIO.cleanup()"
